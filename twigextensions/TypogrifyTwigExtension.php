b93f7cb1769235d7d7196e1f268076df37f7f2bb
Jamie Pittock <jamiepittock@me.com>
2013-11-14 11:08:42 +0000

    Fixes use of require

--- twigextensions/TypogrifyTwigExtension.php
+++ twigextensions/TypogrifyTwigExtension.php
@@ -3,7 +3,7 @@
 namespace Craft;
 
 require_once(CRAFT_PLUGINS_PATH . 'typogrify/lib/smartypants.php');
-require(CRAFT_PLUGINS_PATH . 'typogrify/lib/php-typogrify.php');
+require_once(CRAFT_PLUGINS_PATH . 'typogrify/lib/php-typogrify.php');
 
 class TypogrifyTwigExtension extends \Twig_Extension
 {
